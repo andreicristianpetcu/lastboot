8feb585f51055b935de6ee7d2a48203b464c5e1e
Andrei Cristian Petcu <andrei@ceata.org>
2014-04-15 20:01:47 +0300

    added soft links for less important folders

--- savesynch.rb
+++ savesynch.rb
@@ -9,17 +9,23 @@ current_path="#{save_from}home/"
 puts "Current path is #{current_path}"
 user_include_list=["dotfiles", "Dev"]
 user_exclude_list=[".config/teamviewer9/"]
+
+nondotfile_links = []
+
 Dir.foreach("#{current_path}") do |user|
   if not user.start_with?(".") then
     user_dir="#{current_path}#{user}"
     puts "Processing user #{user_dir}"
     Dir.foreach("#{user_dir}") do |f|
       if not f.start_with?(".") then
+
         if not user_include_list.include?(f) then
           exclude_dir="#{user_dir}/#{f}"
+          nondotfile_links << exclude_dir
           exclude_dir=exclude_dir[save_from.length, exclude_dir.length]
           exclude_file_list="#{exclude_file_list}" + " --exclude '" + "#{exclude_dir}" + "'"
         end
+
       end
     end
     user_exclude_list.each do |user_exclude_dir|
@@ -41,5 +47,13 @@ rsync_cmd="rsync -aAXv --delete #{exclude_file_list} #{save_from} #{save_to}"
 puts "#{rsync_cmd}"
 
 `cp /etc/fstab /etc/fstab.original`
-`#{rsync_cmd}`
+# `#{rsync_cmd}`
 `cp #{save_to}etc/fstab.lastboot #{save_to}/etc/fstab`
+
+puts "links"
+for soft_link in nondotfile_links do
+  source = "/mnt/origin_home" + soft_link
+  destination = save_to + soft_link
+  puts source + " -> " + destination
+  # `ln -s "$source" "$destination"`
+end
